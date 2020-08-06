569ef61a1f5607a5ccdbfdb34e14b4ce132764a2
prafful <prraful@gmail.com>
2020-08-06 00:25:57 -0700

    day 05 - concurrently, npm run eject and npm run build, validation

--- src/components/addfriend.js
+++ src/components/addfriend.js
@@ -7,16 +7,51 @@ class AddFriend extends React.Component {
         super(props)
         this.state ={
             friendname:'',
-            friendsince:0
+            friendsince:0,
+            nameError:'',
+            sinceError:'',
+            buttonStatus: false
         }
+    }
+
+    checkValidation=()=>{
+       
+       let nameerror = ''
+       let sinceerror = ''
+       if(this.state.friendname.includes('#')){
+           console.log('name having #!');
+           nameerror = 'name having invalid #'
+       }
+       //check for other conditions!
+       if(nameerror){
+           console.log('set state for nameError');
+           this.setState({
+                nameError: nameerror,
+                buttonStatus: true
+            })
+           
+           return false
+       }
+
+       this.setState({
+           nameError:'',
+           buttonStatus:false
+        })
+       return true
     }
 
 
     getName=(event)=>{
+        
         console.log(event)
         console.log(event.target)
         console.log(event.target.value)
+        this.checkValidation()
         this.setState({friendname: event.target.value})
+        this.checkValidation()
+
+        
+       
 
     }
 
@@ -51,11 +86,13 @@ class AddFriend extends React.Component {
                 <form>
                     <label>Full Name: </label>
                     <input type='text' id="friendname" onChange={this.getName}></input>
+                    {this.state.nameError}
                     <br></br>
                     <label>Since: </label>
                     <input type='number' id="friendsince" onChange={this.getSince}></input>
+                    {this.state.sinceError}
                     <br></br>
-                    <button type="button" onClick={this.addFriend}>Add Friend</button>
+                    <button type="button" onClick={this.addFriend} disabled={this.state.buttonStatus}>Add Friend</button>
                     <br></br>
                     <div>
                         <h4>Preview</h4>
